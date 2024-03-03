ba61eff3cff40d932ddb4e1f0c13178f034df303
Ruchitha007 <143633364+ruchitha007@users.noreply.github.com>
2024-03-03 18:45:14 -0500

    mongodb

---
+++ server.js
@@ -0,0 +1,75 @@
+const mongoose = require("mongoose");
+const Budget = require("./endpoints/budget_data");
+const bodyParser = require("body-parser");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+};
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+app.use("/", express.static("public"));
+
+const dbName = "budgetData"; 
+const collectionName = "budget"; 
+
+app.get("/budget", (req, res) => {
+  mongoose.connect(`mongodb://127.0.0.1:27017/${dbName}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+    .then(() => {
+      console.log("Connected to the Database");
+      Budget.find({})
+        .then((data) => {
+          res.json(data);
+          console.log(data);
+          mongoose.connection.close();
+        })
+        .catch((connectionError) => {
+          console.error(connectionError);
+        });
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
+
+app.post("/addExpense", (req, res) => {
+
+  mongoose.connect(`mongodb://127.0.0.1:27017/${dbName}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+    .then(() => {
+      console.log("Connected to the database");
+      console.log(req.body);
+
+      const newItem = new Budget(req.body);
+
+      Budget.create(newItem)
+        .then((data) => {
+          res.json(data);
+          console.log(data);
+        })
+        .catch((saveError) => {
+          console.error(saveError);
+          res.status(400).json({ error: 'Internal Server error - Failed to save item' });
+        })
+        .finally(() => {
+          mongoose.connection.close();
+        });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(400).json({ error: 'Internal Server error - Database connection failed' });
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
